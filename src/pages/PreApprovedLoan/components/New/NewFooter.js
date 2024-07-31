ed90bd7d607edc43bb22a560db9dd2425a80175c
Saurabh <saurabh@digitmoney.in>
2024-07-31 11:55:49 +0530

    fixed footer

--- src/pages/PreApprovedLoan/components/New/NewFooter.js
+++ src/pages/PreApprovedLoan/components/New/NewFooter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../New/css/NewFooter.module.css";
+import "./css/NewFooter.css";
 import { Link } from "react-router-dom";
 import { setUserClickData } from "../../../../utility/setUserClickData";
 
@@ -7,329 +7,9 @@ const NewFooter = () => {
   return (
     <section className="footer">
       <div className="container m-auto">
-        <div className="web-footer">
+        <div className="mobile-footer2">
           <div className="row">
-            <div className="col-6 left-side">
-              <div className="logo">
-                <a href="/">
-                  <img src="/assets/img/logo.png" alt="" />
-                </a>
-              </div>
-              <p>Personal Loan Simplified.</p>
-              <h3>COMPANY</h3>
-              <p>
-                <Link
-                  to="/terms"
-                  onClick={() => {
-                    setUserClickData({
-                      event_name: "terms-link",
-                    });
-                  }}
-                >
-                  Terms
-                </Link>
-                |
-                <Link
-                  to="/privacy-policy"
-                  onClick={() => {
-                    setUserClickData({
-                      event_name: "privacy-policy-link",
-                    });
-                  }}
-                >
-                  Privacy Policy
-                </Link>
-                |
-                <Link
-                  to="/contact"
-                  onClick={() => {
-                    setUserClickData({
-                      event_name: "contact-us-link",
-                    });
-                  }}
-                >
-                  Contact Us
-                </Link>
-                |<a href="https://sachet.rbi.org.in/">RBI Sachet</a>
-              </p>
-              <div
-                className="address"
-                onClick={() => {
-                  setUserClickData({
-                    event_name: "about-digit-money-link",
-                  });
-                }}
-              >
-                <a
-                  href="https://www.google.com/maps/place/Regus+-+Mumbai+Navi+Mumbai+Vashi/@19.0656051,72.9938251,17z/data=!3m2!4b1!5s0x3be7c14cc982360b:0xe56cd8202f48699c!4m6!3m5!1s0x3be7c8ee979c7bc9:0xeb3505e4b813f39d!8m2!3d19.0656!4d72.9964!16s%2Fg%2F1hc4583zv?entry=ttu"
-                  target="_blank"
-                >
-                  DigitMoney Technologies Pvt Ltd
-                  <br />
-                  1341 Regus Platinum Business Centre Pvt Ltd,
-                  <br />
-                  Level 13, Platinum Techno Park, <br />
-                  Plot no. 17 &amp; 18, Sector 30A, Vashi, <br />
-                  Navi Mumbai - 400705
-                  <br />
-                </a>
-              </div>
-              <h3>GRIEVANCE REDRESSAL OFFICER DETAILS</h3>
-              <div
-                className="address"
-                onClick={() => {
-                  setUserClickData({
-                    event_name: "grievance-digit-money-link",
-                  });
-                }}
-              >
-                <a href="mailto:grievance@digitmoney.in">
-                  Ammy Pinheiro
-                  <br />
-                  grievance@digitmoney.in
-                </a>
-              </div>
-              <div
-                className="social_icons"
-                onClick={() => {
-                  setUserClickData({
-                    event_name: "social-media-link",
-                  });
-                }}
-              >
-                <a
-                  href="https://www.facebook.com/DigitMoneyTechnologies"
-                  className="social_icon facebook"
-                  target="_blank"
-                  aria-label="Facebook link of DigitMoney"
-                >
-                  <img src="/assets/img/facebook.png" alt="" />
-                </a>
-                <a
-                  href="https://www.instagram.com/digitmoney.india"
-                  className="social_icon instagram"
-                  target="_blank"
-                  aria-label="Instagram link of DigitMoney"
-                >
-                  <img src="/assets/img/instagram.png" alt="" />
-                </a>
-                <a
-                  href="https://twitter.com/DigitMoneyIndia"
-                  className="social_icon twitter"
-                  target="_blank"
-                  aria-label="Twitter link of DigitMoney"
-                >
-                  <img src="/assets/img/twitter.png" alt="" />
-                </a>
-                <a
-                  href="https://www.linkedin.com/company/digitmoney"
-                  className="social_icon linkedin"
-                  target="_blank"
-                  aria-label="LinkedIn link of DigitMoney"
-                >
-                  <img src="/assets/img/linkedin.png" alt="" />
-                </a>
-                <a
-                  href="/"
-                  className="social_icon youtube"
-                  target="_blank"
-                  aria-label="Youtube link of DigitMoney"
-                >
-                  <img src="/assets/img/youtube.png" alt="" />
-                </a>
-                <a
-                  href="/"
-                  className="social_icon threads"
-                  target="_blank"
-                  aria-label="Threads link of DigitMoney"
-                >
-                  <img src="/assets/img/threads.png" alt="" />
-                </a>
-              </div>
-            </div>
-            <div className="col-6 right-side">
-              <div className="card">
-                <div className="mx-0 row">
-                  <div
-                    className="text-center col-4"
-                    onClick={() => {
-                      setUserClickData({
-                        event_name: "whatsapp-link",
-                      });
-                    }}
-                  >
-                    <a href="https://api.whatsapp.com/send/?phone=9076324177&amp;type=phone_number&amp;app_absent=0">
-                      <img
-                        className={"m-auto"}
-                        src="/assets/img/chat.png"
-                        alt=""
-                      />
-                    </a>
-                  </div>
-                  <div
-                    className="text-center col-4"
-                    onClick={() => {
-                      setUserClickData({
-                        event_name: "mail-link",
-                      });
-                    }}
-                  >
-                    <a href="mailto: hello@digitmoney.in">
-                      <img
-                        className={"m-auto"}
-                        src="/assets/img/mail.png"
-                        alt=""
-                      />
-                    </a>
-                  </div>
-                  <div
-                    className="text-center col-4"
-                    onClick={() => {
-                      setUserClickData({
-                        event_name: "digit-money-phone-link",
-                      });
-                    }}
-                  >
-                    <a href="tel:9076324177">
-                      <img
-                        className={"m-auto"}
-                        src="/assets/img/call.png"
-                        alt=""
-                      />
-                    </a>
-                  </div>
-                </div>
-                <div className="mx-0 row">
-                  <div className="text-center col-4">
-                    <p>
-                      {" "}
-                      <a href="https://api.whatsapp.com/send/?phone=9076324177&amp;type=phone_number&amp;app_absent=0">
-                        Chat
-                      </a>
-                    </p>
-                  </div>
-                  <div className="text-center col-4">
-                    <p>
-                      {" "}
-                      <a href="mailto: hello@digitmoney.in">Email</a>
-                    </p>
-                  </div>
-                  <div className="text-center col-4">
-                    <p>
-                      {" "}
-                      <a href="tel:9076324177">Call</a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="row features">
-                <ul className="col-6">
-                  <li>
-                    <a
-                      href="/"
-                      onClick={() => {
-                        setUserClickData({
-                          event_name: "about-digit-money-page",
-                        });
-                      }}
-                    >
-                      About Us
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#faqs"
-                      onClick={() => {
-                        setUserClickData({
-                          event_name: "digit-money-faqs-page",
-                        });
-                      }}
-                    >
-                      FAQs
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      onClick={() => {
-                        setUserClickData({
-                          event_name: "digit-money-blogs-page",
-                        });
-                      }}
-                    >
-                      Blogs
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      onClick={() => {
-                        setUserClickData({
-                          event_name: "digit-money-career-page",
-                        });
-                      }}
-                    >
-                      Career
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/"
-                      onClick={() => {
-                        setUserClickData({
-                          event_name: "digit-money-eligibility-calculator-page",
-                        });
-                      }}
-                    >
-                      Eligibility Calculator
-                    </a>
-                  </li>
-                </ul>
-                <ul className="col-6">
-                  <li
-                    onClick={() => {
-                      setUserClickData({
-                        event_name: "digit-money-affiliate-program-page",
-                      });
-                    }}
-                  >
-                    <a href="/">Affiliate Program</a>
-                  </li>
-                  <li
-                    onClick={() => {
-                      setUserClickData({
-                        event_name: "digit-money-lending-partners-page",
-                      });
-                    }}
-                  >
-                    <a href="lenders">Digital Lending Partners</a>
-                  </li>
-                  <li
-                    onClick={() => {
-                      setUserClickData({
-                        event_name: "digit-money-our-partners-page",
-                      });
-                    }}
-                  >
-                    <a href="/">Our Partners</a>
-                  </li>
-                  <li
-                    onClick={() => {
-                      setUserClickData({
-                        event_name: "digit-money-emi-calculator-page",
-                      });
-                    }}
-                  >
-                    <a href="#emi">EMI Calculator</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mobile-footer">
-          <div className="row">
-            <div className="text-center col-12">
+            <div className="flex justify-center text-center col-12">
               <div className="logo">
                 <a href="index">
                   <img src="/assets/img/logo.png" alt="" />
@@ -400,9 +80,9 @@ const NewFooter = () => {
                 </a>
               </div>
             </div>
-            <div className="text-center col-12">
+            <div className="flex justify-center text-center col-12">
               <div
-                className="social_icons"
+                className="social_icons2"
                 onClick={() => {
                   setUserClickData({
                     event_name: "social-media-link",
@@ -463,11 +143,11 @@ const NewFooter = () => {
                 </a>
               </div>
             </div>
-            <div className="text-center col-12">
-              <div className="card">
+            <div className="flex justify-center text-center col-12">
+              <div className="card2">
                 <div className="mx-0 row">
                   <div
-                    className="px-0 text-center col-4"
+                    className="flex justify-center px-0 text-center col-4"
                     onClick={() => {
                       setUserClickData({
                         event_name: "whatsapp-link",
@@ -479,7 +159,7 @@ const NewFooter = () => {
                     </a>
                   </div>
                   <div
-                    className="px-0 text-center col-4"
+                    className="flex justify-center px-0 text-center col-4"
                     onClick={() => {
                       setUserClickData({
                         event_name: "dm-mail-link",
@@ -491,7 +171,7 @@ const NewFooter = () => {
                     </a>
                   </div>
                   <div
-                    className="px-0 text-center col-4"
+                    className="flex justify-center px-0 text-center col-4"
                     onClick={() => {
                       setUserClickData({
                         event_name: "dm-phone-link",
@@ -511,7 +191,7 @@ const NewFooter = () => {
                     });
                   }}
                 >
-                  <div className="px-0 text-center col-4">
+                  <div className="flex justify-center px-0 text-center col-4">
                     <p>
                       {" "}
                       <a href="https://api.whatsapp.com/send/?phone=9076324177&amp;type=phone_number&amp;app_absent=0">
@@ -519,13 +199,13 @@ const NewFooter = () => {
                       </a>
                     </p>
                   </div>
-                  <div className="px-0 text-center col-4">
+                  <div className="flex justify-center px-0 text-center col-4">
                     <p>
                       {" "}
                       <a href="mailto: hello@digitmoney.in">Email</a>
                     </p>
                   </div>
-                  <div className="px-0 text-center col-4">
+                  <div className="flex justify-center px-0 text-center col-4">
                     <p>
                       {" "}
                       <a href="tel:9076324177">Call</a>
@@ -537,8 +217,8 @@ const NewFooter = () => {
             <div className="mt-3 col-12">
               <hr />
             </div>
-            <div className="col-12">
-              <div className="row features">
+            <div className="items-start justify-start text-start col-12">
+              <div className="row features2">
                 <ul className="px-0 col-6">
                   <li
                     onClick={() => {
@@ -656,7 +336,7 @@ const NewFooter = () => {
             </div>
           </div>
         </div>
-        <div className="row copyright">
+        <div className="row copyright2">
           <div className="text-center col-12">
             © <a href="/">DIGITMONEY</a> 2024{" "}
           </div>
