93ba4a08fe7cfeef7bd1f5a9c0af24428bdc6d22
03-kushagra <kushagra.agrawal@antcreatives.com>
2024-12-02 17:06:00 +0530

    Changes in New form

--- src/pages/PreApprovedNew/PreApprovedNew.js
+++ src/pages/PreApprovedNew/PreApprovedNew.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setLead } from "../../store/app/appReducer";
 import MobileVerification from "./Components/MobileVerification";
 import OffersPage from "./Components/OffersPage";
 import PANVerification from "./Components/PANVerification";
@@ -7,19 +9,30 @@ import WorkDetailsPage from "./Components/WorkDetailsPage";
 import OfferSearchPage from "./Components/OfferSearchPage";
 
 const PreApprovedNew = () => {
-  const [step, setStep] = useState(1);
-  const [userData, setUserData] = useState({
-    mobile: "",
-    pan: "",
-    personalDetails: {},
-    workDetails: {},
-    offers: [],
-  });
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.app.lead); 
+  const [step, setStep] = React.useState(1);
+
+  const handleDataChange = (section, key, value) => {
+    dispatch(setLead({
+      ...userData,
+      [section]: {
+        ...userData[section],
+        [key]: value,
+      },
+    }));
+  };
 
   const steps = [
     {
       id: 1,
-      component: (props) => <MobileVerification {...props} />,
+      component: (props) => (
+        <MobileVerification
+          {...props}
+          userData={userData} 
+          setUserData={handleDataChange} 
+        />
+      ),
     },
     {
       id: 2,
@@ -31,34 +44,44 @@ const PreApprovedNew = () => {
     },
     {
       id: 4,
-      component: (props) => <PersonalDetailsPage {...props} />,
+      component: (props) => (
+        <PersonalDetailsPage
+          {...props}
+          data={userData.personalDetails || {}}
+          handleDataChange={(key, value) =>
+            handleDataChange("personalDetails", key, value)
+          }
+          // setUserData={handleDataChange}
+        />
+      ),
     },
     {
       id: 5,
-      component: (props) => <WorkDetailsPage {...props} />,
+      component: (props) => (
+        <WorkDetailsPage
+          {...props}
+          data={userData.workDetails || {}}
+          handleDataChange={(key, value) =>
+            handleDataChange("workDetails", key, value)
+          }
+        />
+      ),
     },
     {
       id: 6,
       component: (props) => <OfferSearchPage {...props} />,
     },
   ];
+  
 
   const handlePageRender = () => {
     const stepComponent = steps.find((s) => s.id === step);
     return stepComponent
-      ? stepComponent.component({
-          setStep,
-          setUserData,
-          userData,
-        })
+      ? stepComponent.component({ setStep })
       : null;
   };
 
-  return (
-    <div className="preapproved-new-container">
-      {handlePageRender()}
-    </div>
-  );
+  return <div className="preapproved-new-container">{handlePageRender()}</div>;
 };
 
 export default PreApprovedNew;
