7d251bcaa466ee4f30f997d321bcc94363188f6e
supriya <supriya@iperformance.in>
2025-01-13 13:48:46 +0530

    removeicanapicall

--- src/pages/OffersNew/Offerpagenew.js
+++ src/pages/OffersNew/Offerpagenew.js
@@ -25,36 +25,36 @@ const OffersPage = () => {
           res1?.data?.offers?.length > 0
         ) {
           const { lead, offers } = res1.data;
-          const contactPhone =
-            offers.length > 0 ? offers[0].contact_phone : lead.contact_phone;
-          // Transform the lead object
-          const transformedLead = {
-            id: lead._id,
-            contact_name: lead.contact_name || "",
-            contact_phone: contactPhone || "",
-          };
+          // const contactPhone =
+          //   offers.length > 0 ? offers[0].contact_phone : lead.contact_phone;
+          // // Transform the lead object
+          // const transformedLead = {
+          //   id: lead._id,
+          //   contact_name: lead.contact_name || "",
+          //   contact_phone: contactPhone || "",
+          // };
 
-          const transformedOffers = offers.map((offer) => ({
-            lender_name: offer.lender_name,
-            credit_limit: offer.credit_limit,
-            emi: offer.emi,
-            tenure: offer.tenure,
-            app_url: offer.app_url,
-          }));
+          // const transformedOffers = offers.map((offer) => ({
+          //   lender_name: offer.lender_name,
+          //   credit_limit: offer.credit_limit,
+          //   emi: offer.emi,
+          //   tenure: offer.tenure,
+          //   app_url: offer.app_url,
+          // }));
 
-          // Final transformed data for res2
-          const res2Payload = {
-            lead: transformedLead,
-            offers: transformedOffers,
-          };
+          // // Final transformed data for res2
+          // const res2Payload = {
+          //   lead: transformedLead,
+          //   offers: transformedOffers,
+          // };
 
           // Sending to the API
-          const res2 = await callApi(
-            `v1/ican_api/data-send-with-offers-to-ican`,
-            "post",
-            res2Payload,
-            "core"
-          );
+          // const res2 = await callApi(
+          //   `v1/ican_api/data-send-with-offers-to-ican`,
+          //   "post",
+          //   res2Payload,
+          //   "core"
+          // );
 
           // const res3 = await callApi(
           //   `v1/ican_api/data-send-to-ClearTouch`,
