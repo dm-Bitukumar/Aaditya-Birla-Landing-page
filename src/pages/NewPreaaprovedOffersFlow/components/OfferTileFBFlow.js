2f9a39ca32f4b68cafe0347a1ea8b7e52757e369
03-kushagra <kushagra.agrawal@antcreatives.com>
2025-01-30 11:51:49 +0530

    new fb page

---
+++ src/pages/NewPreaaprovedOffersFlow/components/OfferTileFBFlow.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import FormButton from "../../../components/Buttons/FormButton";
+import { convertNumberToIndianFormat } from "../../../utility/numberUtility";
+import { setUserClickData } from "../../../utility/setUserClickData";
+
+const OfferTileNew = ({ offer, small, setStep, mobileNumber }) => {
+  const [show, setShow] = useState(false);
+  const mobile = mobileNumber;
+
+  useEffect(() => {
+    if (show) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [show]);
+
+  const handleClick = () => {
+    setUserClickData({
+      event_name: `fb-offer-apply-button-press`,
+      user_id: mobile || "unknown",
+      segment_name: offer?.lender_name || "unknown",
+    });
+    if (offer?.redirection_link) {
+        window.location.href = offer.redirection_link;
+      }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div
+        className={`rounded-lg ring-1 ring-[#00c0ff] ${
+          small ? "px-0 pb-2" : "px-20 pb-6"
+        } text-center`}
+      >
+        <img width={small ? 100 : 200} src={offer?.logo_image_url} />
+        <h2
+          className={`${small ? "text-sm" : "text-3xl"} font-semibold text-[#00c0ff]`}
+        >   
+          {convertNumberToIndianFormat(offer.loan_amount ?? 0)}
+        </h2>
+        {!small && (
+          <div>
+            <h5 className="mt-6 text-xs font-semibold">
+              Amount: {convertNumberToIndianFormat(offer.loan_amount ?? 0)}
+            </h5>
+            <h5 className="my-2 text-xs font-semibold">
+              Tenure: {offer.loan_tenure} Months
+            </h5>
+            <h5 className="text-xs font-semibold">
+              EMI: {convertNumberToIndianFormat(offer.loan_emi ?? 0)}
+            </h5>
+          </div>
+        )}
+        {!small && (
+          <FormButton
+            small={small}
+            onClick={handleClick}
+            className="!mt-2 !w-40"
+          >
+            APPLY NOW
+          </FormButton>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default OfferTileNew;
