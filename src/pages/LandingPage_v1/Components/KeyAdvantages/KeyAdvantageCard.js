6d3deca35f60da8ca35819b2aa444e284dd224b8
03-kushagra <kushagra.agrawal@antcreatives.com>
2025-03-26 14:10:22 +0530

    landing page changes

---
+++ src/pages/LandingPage_v1/Components/KeyAdvantages/KeyAdvantageCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const KeyAdvantageCard = ({ icon, title, subtitle }) => {
+  return (
+    <div className="advantage-card">
+      <div className="card-icon-wrapper">
+        <img src={icon} alt={title} className="card-icon" />
+      </div>
+
+      <div className="card-content-wrapper">
+        <h4 className="card-title">{title}</h4>
+        <p className="card-subtitle">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default KeyAdvantageCard;
