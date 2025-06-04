15c1086953a11e83c33b0266445a86f40f11758a
Bellal <bellal@ipixel.in>
2025-06-04 16:07:56 +0530

    Landing Pages Changes Done

--- src/pages/OfferCheck_v3/Components/GoogleRatingCard/GoogleRatingCard.js
+++ src/pages/OfferCheck_v3/Components/GoogleRatingCard/GoogleRatingCard.js
@@ -4,26 +4,10 @@ import "./GoogleRatingCard.css";
 const GoogleRatingCard = () => {
   return (
     <div className="google-rating-wrapper">
-      <div className="google-rating-card-ns">
-        <img
-          src="/assets/img/google.png"
-          alt="Google Logo"
-          className="google-logo"
-        />
+      <div className="google-rating-card-v3">
         <div className="google-rating-text">
-          <p className="review-text">
-            <span>1 Lac+ Customers have</span>
-            <br />
-            <span>availed loans with us.</span>
-          </p>
-          <div className="rating-row">
-            <span className="rating-score">4.7</span>
-            <img
-              src="/assets/img/starrating.png"
-              alt="Rating Stars"
-              className="stars-img"
-            />
-          </div>
+          <img src="/assets/img/review_black.png"
+          alt="Google Rating Logo" />
         </div>
       </div>
     </div>
